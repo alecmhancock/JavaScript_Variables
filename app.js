96510232669e7e857612259563e9123ef4f3db6e
Alec Hancock <alec.m.hancock@gmail.com>
2023-04-13 10:10:00 -0500

    Assignment complete

--- app.js
+++ app.js
@@ -2,9 +2,39 @@ console.log("Hello World!\n==========\n");
 console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
-
 // Exercise 1
+const firstName = "Alec";
+let lastName
+var age
 
 // Exercise 2
-
+lastName = "Hancock"
+age = 28
+console.log(firstName, lastName, age)
+console.log(`\n`)
 // Exercise 3
+let language = "Javascript"
+let createdYear = 1995
+let isCaseSensitive = true
+console.log(`${language} was created in ${createdYear} and the fact that it is case sensitive is ${isCaseSensitive}`)
+console.log(`\n`)
+// Exercise 4
+let price = 19.99
+let isOnSale = false
+let salePercentage = 15
+let stock = 0
+let inStock = false
+let selectedSize = "M"
+console.log(`The item is ${price} and a size ${selectedSize} and the fact that it is on sale is ${isOnSale}.`)
+console.log(`The sale percentage would be ${salePercentage}% but we have ${stock} in stock.`)
+console.log(`Again, the fact that this item is in stock is ${inStock}.`)
+console.log(`\n`)
+// Exercise 5
+let title = "Name of the Wind"
+let author = "Patrick Rothfuss"
+let pageCount = 722
+let bookmark = 456
+let hasRead = true
+console.log(`${title},${author}`)
+console.log(`Page count: ${pageCount}, currently on page ${bookmark}`)
+console.log(`It's ${hasRead} I've read this before.`)
